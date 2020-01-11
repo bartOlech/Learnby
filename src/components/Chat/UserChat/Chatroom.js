c2041045e7926673e0cf185fc137deeff2470f64
Bartłomiej Olech <bartlomiejolech@MacBook-Pro-Bartomiej.local>
2020-01-11 22:09:29 +0100

    create input field to chat

--- src/components/Chat/UserChat/Chatroom.js
+++ src/components/Chat/UserChat/Chatroom.js
@@ -3,12 +3,45 @@ import styled from 'styled-components';
 import { ChatFeed } from 'react-bell-chat';
 import { FindAnnouncementConsumer } from '../../../context/CurrentUser.context';
 import Header from './Header';
+import { FontStyle } from '../../../assets/style/style';
+// import smileIco from '../../../assets/img/Mobile/smile.svg';
+import sendIco from '../../../assets/img/Mobile/sendMessage1.svg';
  
 const Container = styled.div`
 
 `
 const Form = styled.form`
-
+    width: 100%;
+    height: 50px;
+    position: absolute;
+    bottom: 0%;
+    display: flex;
+    border-top: 1px solid #D6D6D6;
+`
+const FormBox = styled.div`
+    width: 100%;
+    height: 50px;
+    display: flex;
+`
+const Input = styled.input`
+    width: 100%;
+    height: 50px;
+    border: none;
+    font-family: ${FontStyle.family};
+    color: ${FontStyle.color};
+    font-size: 1.3em;
+    outline: none;
+    padding-left: 10px;
+`
+const SendButton = styled.div`
+    width: 50px;
+    height: 50px;
+    background-image: url(${sendIco});
+    background-repeat: no-repeat;
+    background-size: 30px 30px;
+    margin-top: 10px;
+    margin-left: 10px;
+    cursor: pointer;
 `
 
 class UserChatBox extends Component{
@@ -82,7 +115,12 @@ class UserChatBox extends Component{
                             yourAuthorId={2} // Number: Your author id (corresponds with id from list of authors)
                         />
                         <Form>
-
+                            <FormBox>
+                                <Input placeholder='Napisz wiadomość...'>      
+                                </Input>
+                                {/* <SmileButton></SmileButton> */}
+                                <SendButton></SendButton>
+                            </FormBox>
                         </Form>
                     </Container>
                 )}
