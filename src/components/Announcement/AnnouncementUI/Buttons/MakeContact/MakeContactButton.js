3cfe7e53f204ae1f492eee5fea5faf65d85234da
Bartłomiej Olech <bartlomiejolech@MacBook-Pro-Bartomiej.local>
2020-02-09 16:15:27 +0100

    improve the layout of the selected announcemnet section

--- src/components/Announcement/AnnouncementUI/Buttons/MakeContact/MakeContactButton.js
+++ src/components/Announcement/AnnouncementUI/Buttons/MakeContact/MakeContactButton.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router, Link } from 'react-router-dom';
 import { FindAnnouncementConsumer } from '../../../../../context/CurrentUser.context';
 import SupportIco from '../../../../../assets/img/Desktop/support.svg';
 import SearchPartner from '../../../../Main/User/UserSections/BootomSection/BottomButtons/SearchPartner';
+// media queries
+import Media from 'react-media';
 
 const Container = styled.div`
     width: 100%;
@@ -12,45 +14,19 @@ const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    margin-top: 45px;
-    @media(max-width: 1000px) {
-        margin-top: 100px;
-    }
+    margin-bottom: 100px;
+    /* position: absolute; */
+    /* top: 230px; */
     @media(min-width: 1000px) {
-        width: 300px;
         flex-direction: row;
+        justify-content: flex-end;
         position: absolute;
-        top: 200px;
-        left: -600px; 
-        right: 0; 
-        margin-left: auto; 
-        margin-right: auto;
-
+        right: 35px;
+        top: 230px;
     }
     @media(min-width: 1170px) {
-        width: 300px;
-        position: absolute;
-        top: 0;
-        bottom: 80px;
-        left: 280px;
-        flex-direction: column;
-        margin-left: 0; 
-        margin-right: 0;
-    }
-    @media(min-width: 1200px) {
-        position: absolute;
-        bottom: 80px;
-        left: 290px;
-    }
-    @media(min-width: 1300px) {
-        position: absolute;
-        bottom: 80px;
-        left: 300px;
-    }
-    @media(min-width: 1400px) {
-        position: absolute;
-        bottom: 80px;
-        left: 370px;
+        right: 35px;
+        top: 149px;
     }
 `
 const Button = styled.button`
@@ -69,6 +45,11 @@ const Button = styled.button`
     padding-left: 60px;
     padding-bottom: 5px;
     position: relative;
+    margin-top: 90px;
+    @media(min-width: 1000px) {
+        margin-right: 50px;
+        margin-top: 0;
+    }
 `
 const Ico = styled.div`
     width: 39px;
@@ -106,11 +87,16 @@ const MakeContactButton = (props) => {
                     <Link onClick={() => {
                         createUserChatRoom(selectedAnnouncementData[0].AnnouncementCreator.UserId)
                     }} to={{pathname: `/chat/${props.selectedAnnouncementData[0].AnnouncementCreator.UserId}`}}><Button>Nawiąż współpracę<Ico></Ico></Button></Link>
-                    <BoxLine>
-                        <Line></Line>
-                        <TextLine>Albo</TextLine>
-                        <Line></Line>
-                    </BoxLine>
+                    {/* Phone */}
+                    <Media query="(max-width: 1000px)" render={() =>
+                        (
+                            <BoxLine>
+                                <Line></Line>
+                                <TextLine>Albo</TextLine>
+                                <Line></Line>
+                            </BoxLine>
+                        )}
+                    />
                     <SearchPartner></SearchPartner>
                 </Container>
             )}
